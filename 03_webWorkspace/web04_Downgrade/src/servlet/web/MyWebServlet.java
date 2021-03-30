87217c927bc10d8d7a837a1251f0c5a2ca9bdb1a
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2021-03-30 17:20:32 +0900

    update web04_Downgrade, servlet and web.xml

---
+++ 03_webWorkspace/web04_Downgrade/src/servlet/web/MyWebServlet.java
@@ -0,0 +1,20 @@
+package servlet.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyWebServlet extends HttpServlet {
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		PrintWriter out = res.getWriter();
+		out.println("<h2>Hello MyWebServlet</h2>");
+		out.close();
+	}
+	
+}
