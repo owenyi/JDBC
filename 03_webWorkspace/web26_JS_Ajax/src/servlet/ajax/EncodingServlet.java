de0b846ca802228ee7e4750fec5faf2eee719a3f
LAPTOP-9D74L3IT\oweny <owenyi3207@gmail.com>
2021-04-13 20:32:33 +0900

    update web26 ajax

---
+++ 03_webWorkspace/web26_JS_Ajax/src/servlet/ajax/EncodingServlet.java
@@ -0,0 +1,29 @@
+package servlet.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/EncodingServlet")
+public class EncodingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			
+		}
+		
+		String nick = request.getParameter("nick");
+		out.print(nick);
+	}
+	
+}
