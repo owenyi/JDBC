449ca30797939d4772337331bc954b0ae8dd78e5
owenyi <67588446+owenyi@users.noreply.github.com>
2021-05-29 13:03:02 +0900

    update dp problem1

--- 00_algorithm/Stage1/07_DP/problem1.py
+++ 00_algorithm/Stage1/07_DP/problem1.py
@@ -1,9 +1,16 @@
 # 2579 계단오르기
 
 N = int(input())
-fst = int(input())
-memo = [[0, 0], [fst, fst]]
-for i in range(2, N + 1):
-    nxt = int(input())
-    memo.append([max(memo[i - 2]) + nxt, memo[i - 1][0] + nxt])
+memo = [[0, 0] for _ in range(3)]
+for i in range(3, N + 3):
+    _in = int(input())
+    memo.append([max(memo[i - 2]) + _in, memo[i - 1][0] + _in])
 print(max(memo[-1]))
+
+# N = int(input())
+# fst = int(input())
+# memo = [[0, 0], [fst, fst]]
+# for i in range(2, N + 1):
+#     nxt = int(input())
+#     memo.append([max(memo[i - 2]) + nxt, memo[i - 1][0] + nxt])
+# print(max(memo[-1]))
