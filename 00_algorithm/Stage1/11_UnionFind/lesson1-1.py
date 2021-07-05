10d8108dde7115be2ffa8936a1f6a90d2f0712f9
owenyi <67588446+owenyi@users.noreply.github.com>
2021-07-05 22:54:29 +0900

    update union-find lesson

--- 00_algorithm/Stage1/11_UnionFind/lesson1-1.py
+++ 00_algorithm/Stage1/11_UnionFind/lesson1-1.py
@@ -1,8 +1,8 @@
 # 특정 원소가 속한 집합을 찾기
-def find_parent(parent, x):
+def find_parent(x):
     if parent[x] == x: return x
-    # 루트 노드를 찾을 때까지 재귀 호출
-    return find_parent(parent, parent[x])
+    parent[x] = find_parent(parent[x])
+    return parent[x]
 
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
