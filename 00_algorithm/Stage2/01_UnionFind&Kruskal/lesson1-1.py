7cdca385dea569f6e7ae016ed7716a446eba5bfc
owenyi <67588446+owenyi@users.noreply.github.com>
2021-07-06 20:36:44 +0900

    update unionfind&kruskal lesson

--- 00_algorithm/Stage2/01_UnionFind&Kruskal/lesson1-1.py
+++ 00_algorithm/Stage2/01_UnionFind&Kruskal/lesson1-1.py
@@ -5,9 +5,9 @@ def find_parent(x):
     return parent[x]
 
 # 두 원소가 속한 집합을 합치기
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
+def union_parent(a, b):
+    a = find_parent(a)
+    b = find_parent(b)
     if a < b: parent[b] = a
     else: parent[a] = b
 
@@ -18,12 +18,12 @@ parent = [i for i in range(v + 1)] # 부모 테이블 초기화하기
 # Union 연산을 각각 수행
 for i in range(e):
     a, b = map(int, input().split())
-    union_parent(parent, a, b)
+    union_parent(a, b)
 
 # 각 원소가 속한 집합 출력하기
 print("각 원소가 속한 집합 : ", end='')
 for i in range(1, v + 1):
-    print(find_parent(parent, i), end=' ')
+    print(find_parent(i), end=' ')
 
 print()
 
